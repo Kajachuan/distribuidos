8604753756fd93c7a77b8eaa56c724f6f0541795
Kajachuan <kevincajachuan@hotmail.com>
2019-10-13 13:18:20 -0300

    Filtro de manos diferentes

--- tp2/different_hands_filter/different_hands_filter.py
+++ tp2/different_hands_filter/different_hands_filter.py
@@ -3,32 +3,31 @@
 import pika
 import logging
 
-class AgeDifferenceFilter:
+class DifferentHandsFilter:
     def __init__(self):
         connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
         self.channel = connection.channel()
 
-        self.channel.exchange_declare(exchange='age', exchange_type='direct')
+        self.channel.exchange_declare(exchange='joined', exchange_type='fanout')
         result = self.channel.queue_declare(queue='', exclusive=True)
         queue_name = result.method.queue
-        self.channel.queue_bind(exchange='age', queue=queue_name, routing_key='')
-        self.tag = self.channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=self.filter)
+        self.channel.queue_bind(exchange='joined', queue=queue_name)
 
+        self.tag = self.channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=self.filter)
         self.channel.start_consuming()
 
     def filter(self, ch, method, properties, body):
         logging.info('Received %r' % body)
         data = body.decode().split(',')
-        winner_age = int(data[4])
-        loser_age = int(data[8])
-        if winner_age - loser_age >= 20:
-            winner_name = ' '.join([data[1], data[2]])
-            loser_name = ' '.join([data[5], data[6]])
-            logging.info('%d\t%s\t%d\t%s' % (winner_age, winner_name, loser_age, loser_name))
+        winner_hand = data[3]
+        loser_hand = data[7]
+        if winner_hand in ('R', 'L', 'U') and loser_hand != winner_hand:
+            data = ','.join([winner_hand, loser_hand])
+            logging.info('Sent %s' % data)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(message)s',
                         datefmt='%m/%d/%Y %H:%M:%S',
                         level=logging.INFO)
 
-    filter = AgeDifferenceFilter()
+    filter = DifferentHandsFilter()
